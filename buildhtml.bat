b00164d3b9f84ea059143a665665d07bdacb5368
vtog <v.tognaci@f5.com>
2019-04-10 16:29:12 -0500

    cleanup of build scripts and repo, update to latest f5-sphinx-theme

--- buildhtml.bat
+++ buildhtml.bat
@@ -1,1 +1,1 @@
-docker run --rm -it -e LOCAL_USER_ID=1138664 -v %cd%:/data -w /data f5devcentral/containthedocs:1.0.4_agility make -C docs html
+docker run --rm -it -v %cd%:/data -w /data f5devcentral/containthedocs:1.0.8 make -C docs html
